fc1b1d75b5f6425565ee885d8779511e511b0ef4
Ryan Schildknecht <52866331+rss-618@users.noreply.github.com>
2020-06-03 21:34:46 -0500

    Modern OpenGL Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
-
+	
 	
 
 	/* Make the window's context current */
@@ -27,6 +27,24 @@ int main(void)
 			std::cout << "GLEW IS NOT OKAY!" << std::endl;
 		}
 	std::cout << glGetString(GL_VERSION) << std::endl;
+	//float array of positions
+	float positions[] = {
+		//x, y (opengl doesn't know this though.) -> need to tell opengl the layout after it is bound=(glBindBuffer)
+		-0.5f, -0.5f,//verticies
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+	unsigned int buffer;
+	//generate buffer
+	glGenBuffers(1, &buffer);//passes address so that there is no need to make a copy of value itself?
+	//bind buffer
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); //type , buffer id
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);// <type>,  <howmany> * <size of datatype in bytes>, data being buffered, type of draw(eg.) static or dynamic)
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2,GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //type , buffer id
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -34,11 +52,8 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 		//Create Triangle
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3); //open gl is a state machine (specifier, number of verticies>
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
